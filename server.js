d7267a949db15240e3aaa0b25399894e55f01412
Ohad Atias <OhadA@VoltaBelting.com>
2020-02-16 14:26:08 +0200

    On branch master 	new file:   middleware/auth.js 	modified:   server.js 	new file:   users.txt

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-app.get("/", (req, res) => res.send("Hello From App"));
-const PORT = process.env.PORT || 5000;
+// Connect to DB
+connectDB();
 
+// Init middleware
+//app.use(express.json({ extended: false })); //allows us to get data from request.body
+app.use(express.json()); //pasrse the body to JSON
+app.use(express.urlencoded({ extended: true })); // Parse the URL-encoded
+//app.get("/", (req, res) => res.send("Hello From App"));
+
+//==Define Routes==//
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Is Up On Port ${PORT}`));
